e598a069eb9a7c213e06536572aaf05b3c384e5b
JustinG <justin.gestiere@gmail.com>
2024-08-21 17:58:25 +0200

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 const dropdowns = Array.from(document.querySelectorAll(".dropdown-btn"));
 
-dropdowns.map((dropdown) => {
-  // Code
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", function () {
+    const dropdownContainer = this.nextElementSibling;
+    dropdownContainer.style.display = (dropdownContainer.style.display === "block") ? "none" : "block";
+
+    dropdowns.filter(otherDropdown => otherDropdown !== dropdown)
+             .forEach(otherDropdown => otherDropdown.nextElementSibling.style.display = "none");
+  });
+});
+
+document.querySelectorAll(".dropdown-container a").forEach(link => {
+  link.addEventListener("click", function() {
+    this.closest('.dropdown-container').style.display = "none";
+  });
+});
+
+document.addEventListener("click", function(event) {
+  dropdowns.forEach((dropdown) => {
+    const dropdownContainer = dropdown.nextElementSibling;
+    if (!dropdown.contains(event.target) && !dropdownContainer.contains(event.target)) {
+      dropdownContainer.style.display = "none";
+    }
+  });
 });
